247ea8ca928b60fdca8e95ffd352174c0f51e1a2
ABurkivskyi <aburkivskyi@outlook.com>
2023-08-09 02:53:56 +0200

    Test

--- readme.md
+++ readme.md
@@ -1,5 +1,1 @@
-# Acode plugin
-
-> For typescript version of plugin template switch this repo to main-ts
-
-Read acode plugin [documentation](https://acode.foxdebug.com/plugin-docs) to develop plugin for acode editor.
+# Meslo Font for Acode
--- src/main.js
+++ src/main.js
@@ -1,26 +1,46 @@
 import plugin from '../plugin.json';
 
-class AcodePlugin {
+const pluginId = plugin.id;
+// const appSettings = acode.require('settings');
+const fonts = acode.require('fonts');
+// const { editor } = editorManager;
 
+class Meslo {
   async init() {
-
+    fonts.add(
+      'MesloLGS NF',
+      `@font-face {
+        font-family: "MesloLGS NF";
+        src: url("https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Regular.ttf");
+        font-weight: normal;
+        font-style: normal;
+      }`
+    ),
+    fonts.add(
+      'MesloLGS NF Bold',
+      `@font-face {
+        font-family: "MesloLGS NF";
+        src: url("https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Bold.ttf");
+        font-weight: bold;
+        font-style: normal;
+      }`
+    ),
+    setFont('MesloLGS NF');
   }
-
-  async destroy() {
-
-  }
+  async run() {}
+  async destroy() {}
 }
 
 if (window.acode) {
-  const acodePlugin = new AcodePlugin();
-  acode.setPluginInit(plugin.id, async (baseUrl, $page, { cacheFileUrl, cacheFile }) => {
+  const acodePlugin = new Meslo();
+  acode.setPluginInit(pluginId, async (baseUrl, $page, { cacheFileUrl, cacheFile }) => {
     if (!baseUrl.endsWith('/')) {
       baseUrl += '/';
     }
     acodePlugin.baseUrl = baseUrl;
     await acodePlugin.init($page, cacheFile, cacheFileUrl);
   });
-  acode.setPluginUnmount(plugin.id, () => {
+  acode.setPluginUnmount(pluginId, () => {
     acodePlugin.destroy();
   });
 }
